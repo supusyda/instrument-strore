eb38c87c87a487c3102f7464825a979eed196c7a
supusyda <supusyda123@gmaill.com>
2022-09-25 19:48:46 +0700

    set up connect Database to node

---
+++ backend-instrument/config/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "instrumentstore",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+  },
+};
--- backend-instrument/server.js
+++ backend-instrument/server.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const sequelize = require("./models/index");
 var corOption = {
   origin: "http://localhost::3000",
 };
+//middleware
 app.use(cors(corOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//
+app.get("/", (req, res) => {
+  res.json({ mess: "alo" });
+});
+const PORT = process.env.PORT || 8080;
+const link = `http://localhost:${PORT}`;
+const test = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    sequelize.close();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    sequelize.close();
+  }
+};
+//server
+app.listen(PORT, () => {
+  console.log("your page is running on", link);
+  test();
+});
