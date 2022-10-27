922e437e4e7c985676e6ea3859d2a4c9e3a92454
Duong <93039763+yegiprobuild@users.noreply.github.com>
2022-10-28 04:55:39 +0700

    sidebar admin

--- frontend-instrument/src/components/SlideShow.js
+++ frontend-instrument/src/components/SlideShow.js
@@ -1,6 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 
 const SlideShow = () => {
