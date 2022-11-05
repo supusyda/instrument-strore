cf53b4e697a0dc21b29468f6e67beb292d9941f1
supusyda <supusyda123@gmaill.com>
2022-11-05 15:15:28 +0700

    fix some admin

--- frontend-instrument/src/admin/adminHome.js
+++ frontend-instrument/src/admin/adminHome.js
@@ -6,35 +6,27 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "../screens/Login";
 import Users from "../components/adminComponent/users";
 import Product from "../components/adminComponent/product";
-import Dashboard from "../components/adminComponent/dashboard"
+import Dashboard from "../components/adminComponent/dashboard";
 import AdminHeader from "../components/adminComponent/adminHeader";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 
 const admin = () => {
   return (
     <>
       <div className="admin-contain">
-
-     
-
-        
-        <div>
-
-          <AdminHeader></AdminHeader>{" "}
-          <Switch>
-            <Route path="/admin/started" component={Dashboard} />
-            <Route path="/admin/product" component={Product} />
-            <Route path="/admin/user" component={Users} />
-            <Route path="/admin/product" component={Login} />
-          </Switch>
-          <ToastContainer></ToastContainer>
-        </div>
+        <AdminHeader></AdminHeader>{" "}
         <div className="left-content">
-          {/* <AppLayout></AppLayout> */}
+       
           <Sidebar></Sidebar>
         </div>
+        <Switch>
+          <Route path="/admin/started" component={Dashboard} />
+          <Route path="/admin/product" component={Product} />
+          <Route path="/admin/user" component={Users} />
+          <Route path="/admin/product" component={Login} />
+        </Switch>
+        <ToastContainer></ToastContainer>
       </div>
     </>
   );
--- frontend-instrument/src/components/sidebar/Sidebar.js
+++ frontend-instrument/src/components/sidebar/Sidebar.js
@@ -43,11 +43,15 @@ const Sidebar = () => {
   const handleSidebar = () => {
     setIsOpen(!isOpen);
   };
+  const navLinkStyle = ({ isActive }) => ({
+    color: isActive ? "#fff" : "",
+    backgroundColor: isActive ? "0d6efd" : "",
+  });
+
   return (
     <>
       <nav>
-
-   {*     <input
+        <input
           type="checkbox"
           name="open"
           id="open"
@@ -80,33 +84,32 @@ const Sidebar = () => {
         )}
       </nav>
     </>
-    *}
 
-        <input type="checkbox" name="open" id="open" />
-        <label for="open" class="open">
-          <i class="fas fa-bars"></i>
-        </label>
-        <div className="sidebar">
-          <div className="sidebar__logo"></div>
-          <div ref={sidebarRef} className="sidebar__menu">
-            {sidebarNavItems.map((item, index) => (
-              <NavLink
-                to={item.to}
-                key={index}
-                style={navLinkStyle}
-                className="sidebar__menu__item"
-                exact={index === 0 ? true : false}
-              >
-                <div className="sidebar__menu__item__icon">{item.icon}</div>
-                <div className="sidebar__menu__item__text">{item.display}</div>
-              </NavLink>
-            ))}
-          </div>
-        </div>
-      </nav>
-    </>
-
-
+    // <>
+    //   <nav>
+    //     <input type="checkbox" name="open" id="open" />
+    //     <label for="open" class="open">
+    //       <i class="fas fa-bars"></i>
+    //     </label>
+    //     <div className="sidebar">
+    //       <div className="sidebar__logo"></div>
+    //       <div ref={sidebarRef} className="sidebar__menu">
+    //         {sidebarNavItems.map((item, index) => (
+    //           <NavLink
+    //             to={item.to}
+    //             key={index}
+    //             style={navLinkStyle}
+    //             className="sidebar__menu__item"
+    //             exact={index === 0 ? true : false}
+    //           >
+    //             <div className="sidebar__menu__item__icon">{item.icon}</div>
+    //             <div className="sidebar__menu__item__text">{item.display}</div>
+    //           </NavLink>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </>
   );
 };
 
